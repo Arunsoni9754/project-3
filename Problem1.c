36cd476c48a9328efd9a437026979830c60f07fb
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-26 08:16:14 +0530

    Create Problem1.c

---
+++ Problem1.c
@@ -0,0 +1,8 @@
+#include "Library.h"
+#include <stdio.h>
+#include<conio.h>
+void main()
+{double angle_at_chowk;
+
+angle_at_chowk = find_angle(5,1,-3,2,3,-1);
+printf("Angle at which these roads meets at 123 Chowk: %lf\n",angle_at_chowk);}
