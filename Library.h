b3901dc5125ec303a4579d3ad6eb1233cec4b500
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-26 08:14:58 +0530

    Create Library.h

---
+++ Library.h
@@ -0,0 +1,7 @@
+#ifndef ANGLE_FIND_H_INCLUDED
+#define ANGLE_FIND_H_INCLUDED
+#include "Library.c"
+double find_angle(double,double,double,double,double,double);
+double find_area(double,double,double,double,double,double);
+double find_distance(double,double,double,double);
+#endif
