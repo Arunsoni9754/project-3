74738371ba50e7d447e089120334960934593e87
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-26 08:16:35 +0530

    Create Problem2.c

---
+++ Problem2.c
@@ -0,0 +1,10 @@
+#include "Library.h"
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    double distance;
+   
+    distance=find_distance(23.259933,77.412613,12.9716,77.5946);
+    printf("Distance Between Saurabh Sir and Prateek Sir: %lf\n",distance);
+}
